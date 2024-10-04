ae30d66c6d6e8005f2c7c2c0fbbbc3420513f066
Doriano <doriano.bergamin18@gmail.com>
2024-10-04 21:33:25 +1000

    added slide for reviews

--- src/components/reviews.js
+++ src/components/reviews.js
@@ -3,6 +3,31 @@ import { BlocksRenderer } from "@strapi/blocks-react-renderer";
 import Footer from "./footer";
 import Rating from "./rating";
 import review from "../images/review.png";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const reviews = [
+  {
+    id: 1,
+    name: "Katherine D, Banora Point",
+    review:
+      "Highly recommend Doriano. Our Airbnb was managed seamlessly, with efficient and reliable bookings and communication. And our house was kept in great condition.",
+    rating: 5,
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    review: "Very professional and quick.",
+    rating: 4,
+  },
+  {
+    id: 3,
+    name: "Mike Johnson",
+    review: "I will definitely use them again.",
+    rating: 5,
+  },
+];
 
 export default function Reviews() {
   // const [reviews, setReviews] = useState([]);
@@ -21,6 +46,15 @@ export default function Reviews() {
   //     });
   // }, []);
   // use an empty dependencies array, so we only run the fetch request ONCE
+  const settings = {
+    dots: true,
+    infinite: true,
+    // speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 3000,
+  };
 
   return (
     <>
@@ -52,7 +86,7 @@ export default function Reviews() {
         </h3>
         <container id="star">
           <Rating />
-        </container>
+          {/* </container>
         <p className="reviewname">Katherine D, Banora Point</p>
         <p className="reviews">
           Highly recommend Doriano. Our Airbnb was managed seamlessly, with
@@ -60,7 +94,17 @@ export default function Reviews() {
           kept in great condition.
         </p>
 
-        <container>
+        <container> */}
+          <div>
+            <Slider {...settings}>
+              {reviews.map((review) => (
+                <div key={review.id} className="review-item">
+                  <p className="reviewname">{review.name}</p>
+                  <p className="reviews">{review.review}</p>{" "}
+                </div>
+              ))}
+            </Slider>
+          </div>
           <a
             id="writeareview"
             href="https://www.google.com/maps/place/Homebound+Services/@-30.6637087,152.4510941,7z/data=!4m6!3m5!1s0xef9998617bc7e4d:0xe94329dcf5a091f!8m2!3d-30.6637087!4d152.4510941!16s%2Fg%2F11wfgdzhhf?entry=ttu&g_ep=EgoyMDI0MDkyNS4wIKXMDSoASAFQAw%3D%3D"
